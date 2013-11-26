4dc3c80f8966dfe7b9ec3af659d85f2219ef71ec
Brian Delhaisse <bdelhais@ulb.ac.be>
2013-11-26 15:03:12 +0100

    Update.

--- Authorisation Server/src/connection/AuthorizationService.java
+++ Authorisation Server/src/connection/AuthorizationService.java
@@ -42,16 +42,13 @@ public class AuthorizationService implements Runnable {
 
 	@Override
 	public void run() {
-
 		try {
-
 			initPipeConnection();
+			printKey();
 			sendPubKey();
 			receiveClientPubKey();
 			needhamSchroeder();
 			closeConnection();
-			//printKey();
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
@@ -68,15 +65,37 @@ public class AuthorizationService implements Runnable {
 			e.printStackTrace();
 		}
 	}
-
-	private void printKey() {
-		System.out.println("SERVER: My keys:" + this.rsaKey.getKeyPair());
-		System.out.println("SERVER: My private:" + this.rsaKey.getKeyPair().getPrivate());
-		System.out.println("SERVER: My public:" + this.rsaKey.getKeyPair().getPublic());
-		System.out.println("SERVER: My private:" + this.clientPubKey);
+	
+	private void initPipeConnection() throws IOException {
+		this.input = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+		this.output = new PrintWriter(new OutputStreamWriter(this.clientSocket.getOutputStream()));
+	}
+	
+	private void sendPubKey() throws IOException {
+		ObjectOutputStream outO = new ObjectOutputStream(this.clientSocket.getOutputStream());
+		outO.writeObject(this.rsaKey.getKeyPair().getPublic());
+		outO.flush();
 		
+		System.out.println("SERVER: Public key sent to the client: " + this.rsaKey.getKeyPair().getPublic());
+	}
+	
+	private void receiveClientPubKey() throws IOException, ClassNotFoundException  {
+		ObjectInputStream keyIn = new ObjectInputStream(this.clientSocket.getInputStream());
+		this.clientPubKey = (PublicKey)keyIn.readObject();
+
+		System.out.println("SERVER: Public key received from the client: " + clientPubKey);
 	}
 
+	/**
+	 * Needham-Schroeder protocol.
+	 * @throws InvalidKeyException
+	 * @throws NoSuchAlgorithmException
+	 * @throws NoSuchPaddingException
+	 * @throws IllegalBlockSizeException
+	 * @throws BadPaddingException
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
 	private void needhamSchroeder() throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, IOException, ClassNotFoundException {
 		boolean partnerRecognized = false;
 		Random randomGenerator = new Random();
@@ -89,13 +108,76 @@ public class AuthorizationService implements Runnable {
 		//if (partnerRecognized) System.out.println("Server: Partner recognized");
 		
 		if(this.clientID == 1 && partnerRecognized){
-			System.out.println("Server: Blackboard fully authentified.");
+			System.out.println("SERVER: Blackboard fully authentified.");
+			System.out.println("SERVER: Distribution of the symmetric key...");
 			//createAndSendAES();
-			
 		}
+	}
+	
+	private void receiveIdAndOnce() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException, ClassNotFoundException {
+		Cipher cipher = Cipher.getInstance("RSA");
+		cipher.init(Cipher.DECRYPT_MODE, this.rsaKey.getKeyPair().getPrivate());
+
+		ObjectInputStream in = new ObjectInputStream(this.clientSocket.getInputStream());
+
+		SealedObject clientEncryptedID = (SealedObject)in.readObject();
+		this.clientID = (Integer)clientEncryptedID.getObject(cipher);
+		if(this.clientID == 1){ // Cas du blackboard
+			//System.out.println("Server: BlackBoard detected");
+			SealedObject EncryptedR1 = (SealedObject)in.readObject();
+			this.r1 = (Integer)EncryptedR1.getObject(cipher);
+			//System.out.println(r1);
+		}
+		
+		System.out.println("SERVER : ID received from the client: " + this.clientID);
+		System.out.println("SERVER : R1 received from the client: " + this.r1);
 
 	}
+	
+	private void sendIdAndOncesToService() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, IOException {
+		Cipher cipher = Cipher.getInstance("RSA");
+		cipher.init(Cipher.ENCRYPT_MODE, this.clientPubKey);
+		SealedObject encryptedID = new SealedObject(this.ID, cipher);
+		SealedObject encryptedR1 = new SealedObject(this.r1, cipher);
+		SealedObject encryptedR2 = new SealedObject(this.r2, cipher);
 
+		ObjectOutputStream outO = new ObjectOutputStream(this.clientSocket.getOutputStream());
+		outO.writeObject(encryptedID);
+		outO.flush();
+		outO.writeObject(encryptedR1);
+		outO.flush();
+		outO.writeObject(encryptedR2);
+		outO.flush();
+		
+		System.out.println("SERVER : ID sent to the blackboard: " + this.ID);
+		System.out.println("SERVER : R1 sent to the blackboard: " + this.r1);
+		System.out.println("SERVER : R2 sent to the blackboard: " + this.r2);
+	}
+
+	private boolean receiveOnceBackFromService() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException, ClassNotFoundException {
+		boolean result = false;
+		Cipher cipher = Cipher.getInstance("RSA");
+		cipher.init(Cipher.DECRYPT_MODE, this.rsaKey.getKeyPair().getPrivate());
+
+		ObjectInputStream in = new ObjectInputStream(this.clientSocket.getInputStream());
+
+		SealedObject encryptedR2 = (SealedObject)in.readObject();
+		int receivedR2 = (Integer)encryptedR2.getObject(cipher);
+		if(this.clientID == 1 && receivedR2 == this.r2) result = true;
+
+		System.out.println("R2 received from the client: " + receivedR2);
+		return result;
+	}
+	
+	/**
+	 * AES
+	 * @throws NoSuchAlgorithmException
+	 * @throws NoSuchPaddingException
+	 * @throws IllegalBlockSizeException
+	 * @throws IOException
+	 * @throws InvalidKeyException
+	 * @throws BadPaddingException
+	 */
 	private void createAndSendAES() throws NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, IOException, InvalidKeyException, BadPaddingException {
 		KeyGenerator keyGen = KeyGenerator.getInstance("AES");
 		keyGen.init(128);
@@ -112,90 +194,27 @@ public class AuthorizationService implements Runnable {
 		outO.writeObject(encryptedAESBlackboardKey);
 		outO.flush();
 
-		System.out.println("Server: Blackboard AES key sent.");
+		System.out.println("SERVER: Blackboard AES key sent.");
 
 	}
-
-	private boolean receiveOnceBackFromService() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException, ClassNotFoundException {
-		boolean result = false;
-		Cipher cipher = Cipher.getInstance("RSA");
-		cipher.init(Cipher.DECRYPT_MODE, this.rsaKey.getKeyPair().getPrivate());
-
-		ObjectInputStream in = new ObjectInputStream(this.clientSocket.getInputStream());
-
-		SealedObject encryptedR2 = (SealedObject)in.readObject();
-		int receivedR2 = (Integer)encryptedR2.getObject(cipher);
-		if(this.clientID == 1 && receivedR2 == this.r2) result = true;
-
-		System.out.println("R2 recu du client: " + receivedR2);
-		return result;
+	
+	/**
+	 * Prints the key.
+	 */
+	private void printKey() {
+		System.out.println("SERVER: My keys: " + this.rsaKey.getKeyPair());
+		System.out.println("SERVER: My private: " + this.rsaKey.getKeyPair().getPrivate());
+		System.out.println("SERVER: My public: " + this.rsaKey.getKeyPair().getPublic());
 	}
 
-	private void sendIdAndOncesToService() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, IOException {
-		Cipher cipher = Cipher.getInstance("RSA");
-		cipher.init(Cipher.ENCRYPT_MODE, this.clientPubKey);
-		SealedObject encryptedID = new SealedObject(this.ID, cipher);
-		SealedObject encryptedR1 = new SealedObject(this.r1, cipher);
-		SealedObject encryptedR2 = new SealedObject(this.r2, cipher);
-
-		ObjectOutputStream outO = new ObjectOutputStream(this.clientSocket.getOutputStream());
-		outO.writeObject(encryptedID);
-		outO.flush();
-		outO.writeObject(encryptedR1);
-		outO.flush();
-		outO.writeObject(encryptedR2);
-		outO.flush();
-		
-		System.out.println("SERVER : ID envoyee au blackboard: " + this.ID);
-		System.out.println("SERVER : R1 envoye au blackboard: " + this.r1);
-		System.out.println("SERVER : R2 envoye au blackboard: " + this.r2);
-		
-	}
-
-	private void receiveIdAndOnce() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException, ClassNotFoundException {
-		Cipher cipher = Cipher.getInstance("RSA");
-		cipher.init(Cipher.DECRYPT_MODE, this.rsaKey.getKeyPair().getPrivate());
-
-		ObjectInputStream in = new ObjectInputStream(this.clientSocket.getInputStream());
-
-		SealedObject clientEncryptedID = (SealedObject)in.readObject();
-		this.clientID = (Integer)clientEncryptedID.getObject(cipher);
-		if(this.clientID == 1){ // Cas du blackboard
-			//System.out.println("Server: BlackBoard detected");
-			SealedObject EncryptedR1 = (SealedObject)in.readObject();
-			this.r1 = (Integer)EncryptedR1.getObject(cipher);
-			//System.out.println(r1);
-		}
-		
-		System.out.println("SERVER : ID recue du client: " + this.clientID);
-		System.out.println("SERVER : R1 recu du client: " + this.r1);
-
-	}
-
+	/**
+	 * Closes the connection.
+	 * @throws IOException
+	 */
 	private void closeConnection() throws IOException {
 		this.output.close();
 		this.input.close();
 		this.clientSocket.close();
-	}
-
-	private void receiveClientPubKey() throws IOException, ClassNotFoundException  {
-		ObjectInputStream keyIn = new ObjectInputStream(this.clientSocket.getInputStream());
-		this.clientPubKey = (PublicKey)keyIn.readObject();
-
-		System.out.println("SERVER: Public key received from the client: " + clientPubKey);
-	}
-
-	private void sendPubKey() throws IOException {
-		ObjectOutputStream outO = new ObjectOutputStream(this.clientSocket.getOutputStream());
-		outO.writeObject(this.rsaKey.getKeyPair().getPublic());
-		outO.flush();
-		
-		System.out.println("SERVER: Public key sent to the client: " + this.rsaKey.getKeyPair().getPublic());
-	}
-
-	private void initPipeConnection() throws IOException {
-		this.input = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
-		this.output = new PrintWriter(new OutputStreamWriter(this.clientSocket.getOutputStream()));
 	}
 
 }
