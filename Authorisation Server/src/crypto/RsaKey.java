5d15467e7c2785f346411a80e1c2cd2045b185fa
Anthony Debruyn <antdebru@ulb.ac.be>
2013-11-30 15:55:28 +0100

    Private key reachable.

--- Authorisation Server/src/crypto/RsaKey.java
+++ Authorisation Server/src/crypto/RsaKey.java
@@ -1,18 +1,14 @@
 package crypto;
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.KeyFactory;
 import java.security.KeyPair;
-import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
+import java.security.interfaces.RSAPrivateKey;
 import java.security.spec.InvalidKeySpecException;
-import java.security.spec.KeySpec;
 import java.security.spec.PKCS8EncodedKeySpec;
 
 /**
@@ -29,29 +25,48 @@ public class RsaKey {
 	 * @throws NoSuchAlgorithmException 
 	 */
 	public RsaKey() throws IOException, InvalidKeySpecException, NoSuchAlgorithmException{
+
+		//		File file = new File("src\\chicken.der");
+		//		FileInputStream fis = new FileInputStream(file);
+		//		DataInputStream dis = new DataInputStream(fis);
+		//		byte[] keyBytes = new byte[(int)file.length()];
+		//	    dis.readFully(keyBytes);
+		//	    dis.close();
+		//		
+		//	    PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+		//	    KeyFactory kf = KeyFactory.getInstance("RSA");
+		//	    PrivateKey pk = kf.generatePrivate(spec);
+		//
+		//		System.out.println(spec);
+		//		fis.close();
+
+/*		KeyPairGenerator keyPairGenerator = null;
+		try {
+			keyPairGenerator = KeyPairGenerator.getInstance("RSA"); //RSA
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		keyPairGenerator.initialize(1024); //1024 bits
+		this.keyPair = keyPairGenerator.genKeyPair();*/
+
+		// Loading private key file
 		
-//		File file = new File("src\\chicken.der");
-//		FileInputStream fis = new FileInputStream(file);
-//		DataInputStream dis = new DataInputStream(fis);
-//		byte[] keyBytes = new byte[(int)file.length()];
-//	    dis.readFully(keyBytes);
-//	    dis.close();
-//		
-//	    PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
-//	    KeyFactory kf = KeyFactory.getInstance("RSA");
-//	    PrivateKey pk = kf.generatePrivate(spec);
-//
-//		System.out.println(spec);
-//		fis.close();
-		
-		KeyPairGenerator keyPairGenerator = null;
-        try {
-                keyPairGenerator = KeyPairGenerator.getInstance("RSA"); //RSA
-        } catch (NoSuchAlgorithmException e) {
-                e.printStackTrace();
-        }
-        keyPairGenerator.initialize(1024); //1024 bits
-        this.keyPair = keyPairGenerator.genKeyPair();
+        String keyFile = "src/cakey.p8c";
+        InputStream inStream = new FileInputStream(keyFile);
+        byte[] encKey = new byte[inStream.available()];
+        inStream.read(encKey);
+        inStream.close();
+        
+        // Read the private key from file
+        
+        System.out.println("RSA PrivateKeyInfo: " + encKey.length + " bytes\n");
+        PKCS8EncodedKeySpec privKeySpec = new PKCS8EncodedKeySpec(encKey);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        System.out.println("KeyFactory Object Info:");
+        System.out.println("Algorithm = "+ keyFactory.getAlgorithm());
+        System.out.println("Provider = "+ keyFactory.getProvider());
+        PrivateKey priv = (RSAPrivateKey) keyFactory.generatePrivate(privKeySpec);
+        System.out.println("Loaded " + priv.getAlgorithm() + " " + priv.getFormat() + " private key.");
 	}
 
 	/**
