b1ef6883a35d5aaed0cd81eb83c6714e2c2f247e
auplisni <auplisni@ulb.ac.be>
2013-11-30 18:36:15 +0100

    gg

--- Authorisation Server/src/crypto/RsaKey.java
+++ Authorisation Server/src/crypto/RsaKey.java
@@ -17,14 +17,16 @@ import java.security.interfaces.RSAPrivateKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 
+import dataBase.DbLink;
+
 
 /**
  * Class to create a pair of keys (public and private) RSA.
  */
 public class RsaKey {
 
-     private Certificate cert;
-     private PrivateKey privKey;
+	private Certificate cert;
+	private PrivateKey privKey;
 
 	/**
 	 * Constructor: create a pair of keys (public and private) RSA.
@@ -36,8 +38,8 @@ public class RsaKey {
 	 * @throws NoSuchProviderException 
 	 * @throws InvalidKeyException 
 	 */
+	//public RsaKey(DbLink dbLink) throws IOException, InvalidKeySpecException, NoSuchAlgorithmException, CertificateException, InvalidKeyException, NoSuchProviderException, SignatureException{
 	public RsaKey() throws IOException, InvalidKeySpecException, NoSuchAlgorithmException, CertificateException, InvalidKeyException, NoSuchProviderException, SignatureException{
-
 		//		File file = new File("src\\chicken.der");
 		//		FileInputStream fis = new FileInputStream(file);
 		//		DataInputStream dis = new DataInputStream(fis);
@@ -52,7 +54,7 @@ public class RsaKey {
 		//		System.out.println(spec);
 		//		fis.close();
 
-/*		KeyPairGenerator keyPairGenerator = null;
+		/*		KeyPairGenerator keyPairGenerator = null;
 		try {
 			keyPairGenerator = KeyPairGenerator.getInstance("RSA"); //RSA
 		} catch (NoSuchAlgorithmException e) {
@@ -62,41 +64,42 @@ public class RsaKey {
 		this.keyPair = keyPairGenerator.genKeyPair();*/
 
 		// Loading private key file
-		
-        String keyFile = "src/cakey.p8c";
-        InputStream inStream = new FileInputStream(keyFile);
-        byte[] encKey = new byte[inStream.available()];
-        inStream.read(encKey);
-        inStream.close();
-        
-        // Read the private key from file
-        
-        System.out.println("RSA PrivateKeyInfo: " + encKey.length + " bytes\n");
-        PKCS8EncodedKeySpec privKeySpec = new PKCS8EncodedKeySpec(encKey);
-        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-        System.out.println("KeyFactory Object Info:");
-        System.out.println("Algorithm = "+ keyFactory.getAlgorithm());
-        System.out.println("Provider = "+ keyFactory.getProvider());
-        this.privKey = (RSAPrivateKey) keyFactory.generatePrivate(privKeySpec);
-        System.out.println("Loaded " + this.privKey.getAlgorithm() + " " + this.privKey.getFormat() + " private key.");
-        
-        //Certificate MF !
-        
-        keyFile = "src/cacert.pem";
-        inStream = new FileInputStream(keyFile);
-        CertificateFactory cf = CertificateFactory.getInstance("X.509");
-        this.cert = cf.generateCertificate(inStream);
-        try {
-        	this.cert.verify(cert.getPublicKey());
+
+		String keyFile = "src/cakey.p8c";
+		InputStream inStream = new FileInputStream(keyFile);
+		byte[] encKey = new byte[inStream.available()];
+		inStream.read(encKey);
+		inStream.close();
+
+		// Read the private key from file
+
+		System.out.println("RSA PrivateKeyInfo: " + encKey.length + " bytes\n");
+		PKCS8EncodedKeySpec privKeySpec = new PKCS8EncodedKeySpec(encKey);
+		KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+		System.out.println("KeyFactory Object Info:");
+		System.out.println("Algorithm = "+ keyFactory.getAlgorithm());
+		System.out.println("Provider = "+ keyFactory.getProvider());
+		this.privKey = (RSAPrivateKey) keyFactory.generatePrivate(privKeySpec);
+		System.out.println("Loaded " + this.privKey.getAlgorithm() + " " + this.privKey.getFormat() + " private key.");
+
+		//Certificate MF !
+
+		keyFile = "src/cacert.pem";
+		inStream = new FileInputStream(keyFile);
+		CertificateFactory cf = CertificateFactory.getInstance("X.509");
+		this.cert = cf.generateCertificate(inStream);
+		//this.cert = dbLink.getCertificateByUserID(1);
+		try {
+			this.cert.verify(cert.getPublicKey());
 			System.out.println(cert);
-        } catch (SignatureException e) {
+		} catch (SignatureException e) {
 			e.printStackTrace();
 			System.out.println("You have the wrong key !");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-        
-        
+
+
 	}
 
 	public PublicKey getPubKey(){
