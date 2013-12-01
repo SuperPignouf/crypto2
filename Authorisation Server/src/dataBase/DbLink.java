070da2db705717b4a2283cc10c8f6c0d8dad919c
Brian Delhaisse <bdelhais@ulb.ac.be>
2013-12-01 16:05:16 +0100

    Enable the connection with the DB for mac.

--- Authorisation Server/src/dataBase/DbLink.java
+++ Authorisation Server/src/dataBase/DbLink.java
@@ -1,20 +1,16 @@
 package dataBase;
 
 import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
 import java.io.InputStream;
-import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
-import java.security.spec.X509EncodedKeySpec;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.SQLException;
 
 public class DbLink {
 
@@ -33,7 +29,12 @@ public class DbLink {
 		System.out.println("MySQL JDBC Driver Registered!");
 
 		try {
-			this.con = DriverManager.getConnection("jdbc:mysql://localhost/crypto2","root", "");
+			String OS = System.getProperty("os.name").toLowerCase();
+			if(OS.contains("mac")) {
+				this.con = DriverManager.getConnection("jdbc:mysql://localhost:8889/crypto2","root", "root");
+			} else { //linux or windows
+				this.con = DriverManager.getConnection("jdbc:mysql://localhost/crypto2","root", "");
+			}
 
 		} catch (SQLException e) {
 			System.out.println("Connection Failed! Check output console");
@@ -42,7 +43,7 @@ public class DbLink {
 		}
 
 		if (con != null) {
-			System.out.println("You made it, take control your database now!");
+			System.out.println("You made it, take control of your database now!");
 		} else {
 			System.out.println("Failed to make connection!");
 		}
@@ -65,16 +66,10 @@ public class DbLink {
 				InputStream is = new ByteArrayInputStream(str.getBytes());
 				CertificateFactory cf = CertificateFactory.getInstance("X.509");
 				result = cf.generateCertificate(is);
-				
-				System.out.println("LOOK HERE : " + result.getEncoded().length);
 
-
-			
+				System.out.println("LOOK HERE : " + result.getEncoded().length);			
 			}
-			System.out.println("ICI : " + rs.getString("Certificate"));
-
-			//System.out.println("cctvmb ?");
-
+			System.out.println("HERE : " + rs.getString("Certificate"));
 
 			System.out.println(result);
 
