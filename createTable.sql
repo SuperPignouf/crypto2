15b94de05817bc8d9a657d64f6a84e4cbd9386ac
auplisni <auplisni@ulb.ac.be>
2013-12-01 11:02:06 +0100

    table AS

--- createTable.sql
+++ createTable.sql
@@ -5,7 +5,7 @@ Debruyn Anthony - Plisnier Aurélien - Delhaisse Brian - Lefebvre Alexis
 
 DROP TABLE IF EXISTS Certificates;	
 
-CREATE TABLE Publication (
+CREATE TABLE Certificates (
   ID int(8) NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'The internal key in the database',
   Certificate varchar(1100), COMMENT 'The certificate'
   Time_stp timestamp default current_timestamp COMMENT 'Field used to keep mdate the date of last modification of the record'
